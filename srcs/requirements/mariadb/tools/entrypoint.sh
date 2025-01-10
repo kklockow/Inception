c047b718e92a698e1a151d345e764d8d702f090a
kai <kai.klockow@gmail.com>
2025-01-10 16:06:21 +0100

    now finally works (without mounted volumes) entrypoint.sh (dont forget if clause when mount volumes), everything still really unclean but progress was made i guess

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -6,26 +6,34 @@ chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 chmod -R 755 /var/lib/mysql
 
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+echo ${MYSQL_ROOT_PASSWORD}
+# if [ ! -d "/var/lib/mysql/mysql" ]; then
 
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+echo "Running mysql_install_db"
+mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
 
-    mysqld_safe --skip-networking &
-    # mysqld_safe  &
+mysqld_safe &
+pid="$!"
 
-    pid="$!"
-    sleep 5
+echo "Waiting for MariaDB to be ready..."
+until mysqladmin ping -h localhost --silent; do
+    echo "MariaDB not yet available. Retrying..."
+    sleep 2  # Check every 2 seconds
+done
 
-    mysql --user=root <<-EOSQL
-        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
-        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
-        GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
-        FLUSH PRIVILEGES;
+mysql --user=root <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+    CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+    GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+    FLUSH PRIVILEGES;
 EOSQL
 
-    kill "$pid"
-    wait "$pid"
-fi
+echo "Shutting down MariaDB..."
+mysqladmin -u root -p${MYSQL_ROOT_PASSWORD} shutdown
+
+wait "$pid"
+echo "MariaDB setup complete."
+
+# fi
 
 exec "$@"
